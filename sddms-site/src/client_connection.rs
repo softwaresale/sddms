5149e260ed51e655c7a15418719a7767d51b1e9a
Charlie Sale <softwaresale01@gmail.com>
2023-11-15 15:51:26 -0500

    site: add system to proxy an in memory database
    Each client operates on an in-memory database to avoid sqlite database-level locks. When a connection is created, it opens a "proxy", which essentially copies the disk database into the memory database.
    
    There's also a mechanism for keeping replicating update in different connections. A single set of updates statements can be sent to all connections and update their in memory databases.

--- sddms-site/src/client_connection.rs
+++ sddms-site/src/client_connection.rs
@@ -1,8 +1,10 @@
 use std::collections::HashMap;
 use std::path::Path;
 use std::sync::atomic::{AtomicU32, Ordering};
+use std::time::Duration;
 use log::info;
 use rusqlite::{Connection, OpenFlags};
+use rusqlite::backup::Backup;
 use sddms_services::site_controller::InvokeQueryResults;
 use sddms_shared::error::{SddmsError, SddmsTermError};
 use crate::sqlite_row_serializer::serialize_row;
@@ -92,8 +94,7 @@ impl ClientConnectionMap {
 
     pub fn open_connection(&mut self, db_path: &Path) -> Result<u32, SddmsError> {
         // open connection to database
-        let db_conn = Connection::open(db_path)
-            .map_err(|err| SddmsError::site("Could not open database").with_cause(err))?;
+        let db_conn = Self::open_proxy(db_path)?;
 
         let next_id = self.next_client_id();
 
@@ -101,6 +102,54 @@ impl ClientConnectionMap {
 
         self.connections.insert(next_id, connection);
         Ok(next_id)
+    }
+
+    fn open_proxy(db_path: &Path) -> Result<Connection, SddmsError> {
+        let disk_connection = Connection::open_with_flags(db_path, OpenFlags::SQLITE_OPEN_READ_ONLY)
+            .map_err(|err| SddmsError::site("Could not open disk database").with_cause(err))?;
+
+        let mut memory_connection = Connection::open_in_memory()
+            .map_err(|err| SddmsError::site("Could not open memory database").with_cause(err))?;
+
+        // do this in a smaller scope so that memory_connection borrow drops
+        {
+            let backup = Backup::new(&disk_connection, &mut memory_connection)
+                .map_err(|err| SddmsError::site("Failed to create backup").with_cause(err))?;
+
+            backup.run_to_completion(5, Duration::from_millis(500), None)
+                .map_err(|err| SddmsError::site("Error while backing up").with_cause(err))?;
+        }
+
+        Ok(memory_connection)
+    }
+
+    pub async fn replicate_messages(&self, update_stmts: &[String], skip_site: Option<u32>) -> Result<(), SddmsError> {
+        for (site_id, connection) in &self.connections {
+            // skip the site we don't want
+            if skip_site.is_some_and(|skipped_id| *site_id == skipped_id) {
+                continue;
+            }
+
+            // invoke the sql text on the
+            Self::perform_update_transaction(update_stmts, connection).await?;
+        }
+
+        Ok(())
+    }
+
+    async fn perform_update_transaction(stmts: &[String], connection: &ClientConnection) -> Result<(), SddmsError> {
+
+        let connection = connection.connection.lock().await;
+
+        for stmt in stmts {
+            let execute_result = connection.execute(stmt, []);
+            if let Err(error) = execute_result {
+                let err = SddmsError::site("Failed to execute update statement")
+                    .with_cause(error);
+                return Err(err);
+            }
+        }
+        Ok(())
     }
 
     pub fn get_client_connection(&self, client_id: u32) -> Option<&ClientConnection> {
