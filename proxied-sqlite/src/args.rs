b3fc4e43f2e64957158ab3c60e0a198472d249e7
Charlie Sale <softwaresale01@gmail.com>
2023-11-16 17:01:52 -0500

    ditch proxied sqlite

--- proxied-sqlite/src/args.rs
+++
@@ -1,9 +0,0 @@
-use std::path::PathBuf;
-use clap::Parser;
-
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-pub struct Args {
-    /// path to the sqlite db to open. Creates if it doesn't exist
-    pub db_path: PathBuf,
-}
