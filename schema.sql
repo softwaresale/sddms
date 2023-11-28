910e7f67787cd2d2690bc39fe4bfa6eabe8adf6c
Charlie Sale <softwaresale01@gmail.com>
2023-11-28 11:58:29 -0500

    schema: clear data, ignore create if already exists

--- schema.sql
+++ schema.sql
@@ -1,19 +1,21 @@
 
+-- do all of this in a transaction
 BEGIN TRANSACTION;
 
-CREATE TABLE students (
+-- create each table if they don't already exist
+CREATE TABLE IF NOT EXISTS students (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     gpa FLOAT CHECK ( 0.0 <= gpa <= 4.0)
 );
 
-CREATE TABLE professors (
+CREATE TABLE IF NOT EXISTS professors (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     years_teaching INTEGER
 );
 
-CREATE TABLE grades (
+CREATE TABLE IF NOT EXISTS grades (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     course_code TEXT,
     grade INTEGER CHECK ( 0 <= grade <= 10 ),
@@ -21,7 +23,7 @@ CREATE TABLE grades (
     FOREIGN KEY(student_id) REFERENCES students(id)
 );
 
-CREATE TABLE classes (
+CREATE TABLE IF NOT EXISTS classes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     class_name TEXT,
     enroll_count INTEGER,
@@ -29,6 +31,14 @@ CREATE TABLE classes (
     FOREIGN KEY (teacher_id) REFERENCES professors(id)
 );
 
+-- delete all data from tables
+DELETE FROM sqlite_sequence;
+DELETE FROM students;
+DELETE FROM professors;
+DELETE FROM grades;
+DELETE FROM classes;
+
+-- insert default data
 INSERT INTO students (name, gpa)
     VALUES
         ('test1', 3.4),
